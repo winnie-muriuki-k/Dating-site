4cab48627700c7b9f94599eff9217da5bb580c8e
Winnie-Muriuki <48149892+Winnie-Muriuki@users.noreply.github.com>
2019-08-03 09:57:43 +0300

    profile updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+use App\Matches;
+use App\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Auth;
+use View as BaseView;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +29,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        BaseView::composer('*', function($view)
+        {
+            if(Auth::check()){
+                $confirmed_recent_activity=  Matches::where(['initator'=>Auth::user()->id,'confirmed'=>'1'])->where('recipient', '!=', Auth::user()->id )->latest()->limit(6)->get();
+                $viewed_profiles = View::where(['viewer'=>Auth::user()->id])->latest()->limit(3)->get();
+                $my_viewers = View::where(['recipient'=>Auth::user()->id])->latest()->limit(4)->get();
+                $view->with(['confirmed_recent_activity'=>$confirmed_recent_activity, 'viewed_profiles'=>$viewed_profiles,'my_viewers'=>$my_viewers]);
+            }
+
+        });
         Schema::defaultStringLength(191);
     }
 }
